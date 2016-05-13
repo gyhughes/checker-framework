b6337dcc913f378fe5320c53436703e765b5162a
jsantino <jsantino@sparta.cs.washington.edu>
2016-05-13 16:42:44 -0700

    debugging stuff

---
+++ checker/src/deduggingIndex/forloop.java
@@ -0,0 +1,14 @@
+package deduggingIndex;
+public class forloop {
+
+	public static void main() {
+		int[] arr = new int[100];
+		int l = arr.length;
+		int i = 0;
+		while (i < l) {
+			arr[i] = 1;
+			i++;
+		}
+	}
+
+}
