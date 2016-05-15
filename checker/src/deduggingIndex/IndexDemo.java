0bad459f3c24d1d4db8129d1cc67aad9546b5916
jsantino <jsantino@cs.washington.edu>
2016-05-14 18:56:44 -0700

    add a demo file

---
+++ checker/src/deduggingIndex/IndexDemo.java
@@ -0,0 +1,49 @@
+package deduggingIndex;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class IndexDemo {
+	public static void main(String[] args) {
+		Random r = new Random();
+		int[] arr = new int[100];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = r.nextInt(9)+1;
+		}
+		Scanner con = new Scanner(System.in);
+		System.out.println("input a number between 1 and 10");
+		int num = con.nextInt();
+		firstocc(arr, num);
+		lastOcc(arr, num);
+		numOcc(arr, num);
+	}
+
+	private static void numOcc(int[] arr, int num) {
+		int count = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] == num) {
+				count++;
+			}
+		}
+		System.out.println(num + " occurs " + count + " times");
+		
+	}
+
+	private static void lastOcc(int[] arr, int num) {
+		for (int i = arr.length - 1; i > -1; i++) {
+			if(arr[i] == num) {
+				System.out.println("Last Occurence of" + num + " is at " + i);
+				return;
+			}
+		}
+	}
+
+	private static void firstocc(int[] arr, int num) {
+		for (int i = 0; i <= arr.length; i++) {
+			if(arr[i] == num) {
+				System.out.println("First Occurence of " + num + " is at " + i);
+				return;
+			}
+		}		
+	}
+}
