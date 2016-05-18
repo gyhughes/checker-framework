251682204a8eee5cca1e6fc59141224cfbaa91e1
jsantino <jsantino@quattro.cs.washington.edu>
2016-05-17 17:08:55 -0700

    more LUB tests

---
+++ checker/tests/index/index-hierarchy/LUBIndexFor.java
@@ -0,0 +1,55 @@
+import org.checkerframework.checker.index.qual.*;
+
+
+public class LUBIndexFor {
+
+	//:: error: (assignment.type.incompatible)
+	@IndexFor("a") int InF = 3;
+	//:: error: (assignment.type.incompatible)
+	@IndexOrLow("a") int IOL = 4;
+	//:: error: (assignment.type.incompatible)
+	@IndexOrHigh("a") int IOH = 2;
+	//:: error: (assignment.type.incompatible)
+	@LTLength("a") int LTL = 6; 
+	
+	//:: error: (assignment.type.incompatible)
+	@IndexFor("b") int InFb = 3;
+	//:: error: (assignment.type.incompatible)
+	@IndexOrLow("b") int IOLb = 4;
+	//:: error: (assignment.type.incompatible)
+	@IndexOrHigh("b") int IOHb = 2;
+	//:: error: (assignment.type.incompatible)
+	@LTLength("b") int LTLb = 6;
+
+	@Unknown int Unk = -4;
+	@NonNegative int NN = 8;
+	boolean bool;
+	
+	// should be NonNegative
+	void InFandIOHb() {
+		int i = bool ? InF : IOHb;
+		//:: error: (assignment.type.incompatible)
+		IOH = i;
+		//:: error: (assignment.type.incompatible)
+		IOL = i;
+		//:: error: (assignment.type.incompatible)
+		LTL = i;
+		NN = i;
+		Unk = i;
+	}
+	
+	// should be unknown
+	void InFandIOLb() {
+		int i = bool ? InF : IOLb;
+		//:: error: (assignment.type.incompatible)
+		IOH = i;
+		//:: error: (assignment.type.incompatible)
+		IOL = i;
+		//:: error: (assignment.type.incompatible)
+		LTL = i;
+		//:: error: (assignment.type.incompatible)
+		NN = i;
+		Unk = i;
+	}
+	
+}
---
+++ checker/tests/index/index-hierarchy/LUBOneArray.java
@@ -0,0 +1,64 @@
+import org.checkerframework.checker.index.qual.*;
+
+public class LUBOneArray {
+	int[] arr = new int[10];
+	//:: error: (assignment.type.incompatible)
+	@IndexFor("arr") int InF = 3;
+	//:: error: (assignment.type.incompatible)
+	@IndexOrLow("arr") int IOL = 4;
+	//:: error: (assignment.type.incompatible)
+	@IndexOrHigh("arr") int IOH = 2;
+	@Unknown int Unk = -4;
+	@NonNegative int NN = 8;
+	//:: error: (assignment.type.incompatible)
+	@LTLength("arr") int LTL = 6; 
+	boolean bool;
+	
+	void IOHandIOL() {
+		int index = bool ? IOH : IOL;
+		//:: error: (assignment.type.incompatible)
+		IOH = index;
+		//:: error: (assignment.type.incompatible)
+		IOL = index;
+		//:: error: (assignment.type.incompatible)
+		LTL = index;
+		//:: error: (assignment.type.incompatible)
+		NN = index;
+		// should be unknown
+		Unk = index;
+	}
+	void IOLandIOL() {
+		int index = bool ? IOL : IOL;
+		//:: error: (assignment.type.incompatible)
+		IOH = index;
+		IOL = index;
+		LTL = index;
+		//:: error: (assignment.type.incompatible)
+		NN = index;
+		// should be unknown
+		Unk = index;
+	}
+	void IOHandIOH() {
+		int index = bool ? IOH : IOH;
+		IOH = index;
+		//:: error: (assignment.type.incompatible)
+		IOL = index;
+		//:: error: (assignment.type.incompatible)
+		LTL = index;
+		NN = index;
+		// should be unknown
+		Unk = index;
+	}
+	void InFandIOL() {
+		int index = bool ? InF : IOL;
+		//:: error: (assignment.type.incompatible)
+		IOH = index;
+		IOL = index;
+		LTL = index;
+		//:: error: (assignment.type.incompatible)
+		NN = index;
+		// should be unknown
+		Unk = index;
+	}
+
+}
