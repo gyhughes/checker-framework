068d48d7287fe66cb25c83473f00ad242c2ee4f0
GRANT YUZON HUGHES <gyhughes@cs.washington.edu>
2016-05-19 16:25:44 -0700

    Added type rules for List access. Fixed warning syntax errors in IV.java

---
+++ checker/tests/index/index-lists/ListAccess.java
@@ -0,0 +1,35 @@
+import org.checkerframework.checker.index.qual.*;
+import java.util.*;
+
+class ListAccess {
+
+	void accessIndexFor(List<Integer> lst, @IndexFor("lst") int i) {
+		Integer result = lst.get(i);
+	}
+	
+	void accessIndexOrHigh(List<Integer> lst, @IndexOrHigh("lst") int i) {
+		//:: warning: (Potentially unsafe list access: only use @IndexFor as index. Found: @IndexOrHigh("lst") int)
+		Integer result = lst.get(i);
+	}
+	
+	void accessIndexOrLow(List<Integer> lst, @IndexOrLow("lst") int i) {
+		//:: warning: (Potentially unsafe list access: only use @IndexFor as index. Found: @IndexOrLow("lst") int)
+		Integer result = lst.get(i);
+	}
+	
+	void accessNonNegative(List<Integer> lst, @NonNegative int i) {
+		//:: warning: (Potentially unsafe list access: only use @IndexFor as index. Found: @NonNegative int)
+		Integer result = lst.get(i);
+	}
+	
+	void accessLTLength(List<Integer> lst, @LTLength("lst") int i) {
+		//:: warning: (Potentially unsafe list access: only use @IndexFor as index. Found: @LTLength("lst") int)
+		Integer result = lst.get(i);
+	}
+	
+	void accessUnknown(List<Integer> lst, @Unknown int i) {
+		//:: warning: (Potentially unsafe list access: only use @IndexFor as index. Found: @Unknown int)
+		Integer result = lst.get(i);
+	}
+
+}
