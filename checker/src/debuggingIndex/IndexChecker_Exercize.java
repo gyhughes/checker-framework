9e072a2259178286ffbeae652f8a4b8cdd1e4090
Wesley Cox <coxw2@hughd.cs.washington.edu>
2016-05-23 19:05:15 -0700

    fixed exercize to be compilable (got rid of package statement, renamed classes

--- checker/src/debuggingIndex/IndexChecker_Exercize.java
+++ checker/src/debuggingIndex/IndexChecker_Exercize.java
@@ -1,10 +1,8 @@
-package debuggingIndex;
-
 import java.util.Random;
 import java.util.Scanner;
 import org.checkerframework.checker.index.qual.*;
 
-public class IndexDemo_wSuppWarn {
+public class IndexChecker_Exercize {
 
 	/**
 	 * There are two errors in this class related to indexing. Try to find them.
@@ -38,9 +36,7 @@ public class IndexDemo_wSuppWarn {
 
 	//print the index of the first occurance of num in arr
 	private static void firstocc(int[] arr, int num) {
-		//ERROR: incrementing up to arr.length instead of arr.length - 1
-	  //for (int i = 0; i <= arr.length; i++) {
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i <= arr.length; i++) {
 			if (arr[i] == num) {
 				System.out.println("First Occurence of " + num + " is at " + i);
 				return;
@@ -50,9 +46,7 @@ public class IndexDemo_wSuppWarn {
 
 	//print the index of the last occurance of num in arr
 	private static void lastOcc(int[] arr, int num) {
-		//ERROR: trying to increment up whilst at the end of the array
-	  //for (int i = arr.length - 1; i > -1; i++) {
-		for (int i = arr.length - 1; i > -1; i--) { 
+		for (int i = arr.length - 1; i > -1; i++) {
 			if (arr[i] == num) {
 				System.out.println("Last Occurence of" + num + " is at " + i);
 				return;
@@ -66,19 +60,7 @@ public class IndexDemo_wSuppWarn {
 			throw new IllegalArgumentException();
 
 		for (int i = 0; i < 10; i++) {
-
-			//FALSE WARNING: the IndexChecker does not keep track of array lengths,
-			//	just whether or not indecies were compared against array bounds.
-			//System.out.println("arr[" + i + "] == " + arr[i]);
-
-			// Option 1: add redudant bound checking
-			//if (i < arr.length)
-			//	System.out.println("arr[" + i + "] == " + arr[i]);
-
-			// Option 2: Cast to local @IndexFor variable and suppress warning
-			@SuppressWarnings("index")
-			@IndexFor("arr") int j = i;
-			System.out.println("arr[" + i + "] == " + arr[j]);
+			System.out.println("arr[" + i + "] == " + arr[i]);
 		}		
 	}
 }
