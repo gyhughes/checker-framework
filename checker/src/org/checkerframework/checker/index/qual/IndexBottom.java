2b769f799b339157da8851bf4db64b3a7798a508
jsantino <jsantino@sparta.cs.washington.edu>
2016-06-02 13:13:51 -0700

    update indexBottom to be default for lower bounds

--- checker/src/org/checkerframework/checker/index/qual/IndexBottom.java
+++ checker/src/org/checkerframework/checker/index/qual/IndexBottom.java
@@ -1,11 +1,21 @@
 package org.checkerframework.checker.index.qual;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+import org.checkerframework.framework.qual.DefaultFor;
 import org.checkerframework.framework.qual.ImplicitFor;
 import org.checkerframework.framework.qual.LiteralKind;
 import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TargetLocations;
+import org.checkerframework.framework.qual.TypeUseLocation;
 
 @SubtypeOf({IndexFor.class, MinLen.class})
-@ImplicitFor(literals = {LiteralKind.NULL},
-typeNames = {java.lang.Void.class})
+@Target({ ElementType.TYPE_USE, ElementType.TYPE_PARAMETER })
+@TargetLocations({ TypeUseLocation.EXPLICIT_LOWER_BOUND,
+    TypeUseLocation.EXPLICIT_UPPER_BOUND })
+@ImplicitFor(literals = { LiteralKind.NULL },
+  typeNames = {java.lang.Void.class})
+@DefaultFor({ TypeUseLocation.LOWER_BOUND })
 public @interface IndexBottom {
 }
