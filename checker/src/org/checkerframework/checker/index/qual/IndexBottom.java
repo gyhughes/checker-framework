3a57732f5d2f7c53d71c3a8371b54b8d31394c4d
jsantino <jsantino@sparta.cs.washington.edu>
2016-06-02 12:42:53 -0700

    make bottom implicitFor null

--- checker/src/org/checkerframework/checker/index/qual/IndexBottom.java
+++ checker/src/org/checkerframework/checker/index/qual/IndexBottom.java
@@ -1,7 +1,11 @@
 package org.checkerframework.checker.index.qual;
 
+import org.checkerframework.framework.qual.ImplicitFor;
+import org.checkerframework.framework.qual.LiteralKind;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 @SubtypeOf({IndexFor.class, MinLen.class})
+@ImplicitFor(literals = {LiteralKind.NULL},
+typeNames = {java.lang.Void.class})
 public @interface IndexBottom {
 }
