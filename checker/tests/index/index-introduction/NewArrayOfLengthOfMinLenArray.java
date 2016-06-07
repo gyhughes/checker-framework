459a7c50d138f95ed2f09a1fc86a2d908c7e47a8
Michael Ernst <mernst@cs.washington.edu>
2016-06-07 15:57:01 -0700

    Tests should not use annotations in comments

--- checker/tests/index/index-introduction/NewArrayOfLengthOfMinLenArray.java
+++ checker/tests/index/index-introduction/NewArrayOfLengthOfMinLenArray.java
@@ -6,7 +6,7 @@ import org.checkerframework.checker.index.qual.MinLen;
 
 public class NewArrayOfLengthOfMinLenArray {
 
-  int /*@MinLen(1)*/ [] m(int /*@MinLen(1)*/ [] nums) {
+  int @MinLen(1) [] m(int @MinLen(1) [] nums) {
     return new int[nums.length];
   }
 
