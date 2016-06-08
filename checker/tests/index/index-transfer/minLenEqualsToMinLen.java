542d5087d964acbb287817c8c15ec93a716f0aba
Joe Santino <jsantino@cs.washington.edu>
2016-06-07 17:35:41 -0700

    fix syntax in minlen = test

--- checker/tests/index/index-transfer/minLenEqualsToMinLen.java
+++ checker/tests/index/index-transfer/minLenEqualsToMinLen.java
@@ -2,7 +2,7 @@ import org.checkerframework.checker.index.qual.*;
 import java.util.List;
 
 class minLenEqualsToMinLen {
-	void foo(int @MinLen(2) [] arr, int[] arr2, int @MinLen(1) arr3) {
+	void foo(int @MinLen(2)[] arr, int[] arr2, int @MinLen(1)[] arr3) {
 		// get a minlen from an array if equals is true
 		if (arr2.length == arr.length) {
 			int m = arr2[1];
