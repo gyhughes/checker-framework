423512d6f9facbc5bb543e5d254fe61ef64d3522
Michael Ernst <mernst@cs.washington.edu>
2016-06-27 00:01:48 -0700

    Add test case for issue #28: https://github.com/gyhughes/checker-framework/issues/28

---
+++ checker/tests/index/index-introduction/Issue28.java
@@ -0,0 +1,12 @@
+// Test case for issue #28:
+// https://github.com/gyhughes/checker-framework/issues/28
+
+// @skip-test until the issue is resolved
+
+class Issue28 {
+
+  public void fn_is_permutation(int[] a) {
+    boolean[] see = new boolean[a.length];
+  }
+
+}
